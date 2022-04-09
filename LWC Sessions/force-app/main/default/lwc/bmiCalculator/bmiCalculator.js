101d4301832526566bae89142d896d30c97a520d
Prakash Das <prakashdas18@gmail.com>
2022-04-09 11:31:31 +0530

    getter properties

--- LWC Sessions/force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ LWC Sessions/force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -3,34 +3,42 @@ import { LightningElement, track } from 'lwc';
 export default class BmiCalculator extends LightningElement {
     cardTitle = 'BMI Calculator';
 
-    @track bmiData = {
-        weight: 0,
-        height: 0,
-        result: 0
-    };
+    // @track bmiData = {
+    //     weight: 0,
+    //     height: 0,
+    //     result: 0
+    // };
 
-    //weight;
-    //height;
+    weight;
+    height;
 
     // @track bmi;
     bmi;
 
     onWeightChange(event){
-        // this.weight = parseFloat(event.target.value);
-        this.bmiData.weight = parseFloat(event.target.value);
+        this.weight = parseFloat(event.target.value);
+        // this.bmiData.weight = parseFloat(event.target.value);
     }
 
     onHeightChange(event){
-        // this.height = parseFloat(event.target.value);
-        this.bmiData.height = parseFloat(event.target.value);
+        this.height = parseFloat(event.target.value);
+        // this.bmiData.height = parseFloat(event.target.value);
     }
 
     calculateBMI(){
         try{
-        // this.bmi = this.weight/(this.height*this.height);
-        this.bmiData.result = this.bmiData.weight/(this.bmiData.height*this.bmiData.height);
+        this.bmi = this.weight/(this.height*this.height);
+        // this.bmiData.result = this.bmiData.weight/(this.bmiData.height*this.bmiData.height);
         } catch(error){
-            this.bmiData.bmi = undefined;
+            this.bmi = undefined;
+            // this.bmiData.bmi = undefined;
         }
+    }
+
+    get bmiValue() {
+        if(this.bmi === undefined) {
+            return "";
+        }
+        return `Your BMI is ${this.bmi}`;
     }
 }
