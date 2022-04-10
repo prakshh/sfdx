849414ddf48d0dc5411bd0a492227ba164f10116
Prakash Das <prakashdas18@gmail.com>
2022-04-10 19:24:42 +0530

    public Methods

---
+++ LWC Sessions/force-app/main/default/lwc/publicMethodParent/publicMethodParent.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class PublicMethodParent extends LightningElement {
+    value;
+    checkboxSelectHandler() {
+        const childComponent = this.template.querySelector('c-public-method-child');
+        const returnedMessage = childComponent.selectCheckbox(this.value);
+        console.log('Returned Message : ', returnedMessage);
+    }
+
+    onInputChangeHandler(event) {
+        this.value = event.target.value;
+    }
+}
