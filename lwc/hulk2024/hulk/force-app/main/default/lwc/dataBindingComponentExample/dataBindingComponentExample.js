cad0f8e4befe9ca127f5a04c658e9c8c7eba7de7
prakshh <prakashdas18@gmail.com>
2024-12-29 11:33:14 +0530

    data binding

---
+++ lwc/hulk2024/hulk/force-app/main/default/lwc/dataBindingComponentExample/dataBindingComponentExample.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class DataBindingComponentExample extends LightningElement {
+    firstName = "Ballerina Wick";
+}
+
+/*
+data binding
+    - process of transferring (displaying) data from js to html
+*/
