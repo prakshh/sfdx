8cf4c1675a250c325a62e468221132b8e9e52ca7
prakshh <prakashdas18@gmail.com>
2025-01-08 19:13:16 +0530

    display number of records according to user input using event

--- lwc/hulk2024/hulk/force-app/main/default/lwc/highRevenueAccountUsingImperative/highRevenueAccountUsingImperative.js
+++ lwc/hulk2024/hulk/force-app/main/default/lwc/highRevenueAccountUsingImperative/highRevenueAccountUsingImperative.js
@@ -3,11 +3,24 @@ import getHighRevenueAccountRecordsUsingImperativeCall from '@salesforce/apex/Ac
 
 export default class HighRevenueAccountUsingImperative extends LightningElement {
     accountsToDisplay = [];
-    countOfRecords = 5;
+    countOfRecords = 5;     // here, 5 is assigned a s default value of number of items to be displayed. But, in case of imperative call, whatever input value will be entered, that number of items will be displayed
 
     connectedCallback() {
         getHighRevenueAccountRecordsUsingImperativeCall( {count: this.countOfRecords} ).then(response => {
             console.log('Response using imperativeapproach', response);
+            this.accountsToDisplay = response;
+        }).catch(error => {
+            console.error('Error', error);
+        })
+    }
+
+    setCount(event) {
+        console.log('Value', event.target.value);
+        let inputValue = event.target.value;
+        if(inputValue == '') return;
+        this.countOfRecords = inputValue;
+        getHighRevenueAccountRecordsUsingImperativeCall( {count: this.countOfRecords} ).then(response => {
+            console.log('Response using imperative approach', response);
             this.accountsToDisplay = response;
         }).catch(error => {
             console.error('Error', error);
